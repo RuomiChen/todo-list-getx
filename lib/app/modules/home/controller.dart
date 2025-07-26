7ef815d0580c05327b9130ce56508f8f3e8b324e
zihao.chen <zihao.chen@xilancloud.com>
2025-07-26 11:12:45 +0800

    feat: enhance AddCard widget with dialog for task creation and validation

--- lib/app/modules/home/controller.dart
+++ lib/app/modules/home/controller.dart
@@ -1,21 +1,27 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_application_1/app/data/models/task.dart';
 import 'package:flutter_application_1/app/data/services/storage/repository.dart';
 import 'package:get/get.dart';
 
-class HomeController extends GetxController{
+class HomeController extends GetxController {
   TaskRepository taskRepository;
   HomeController({required this.taskRepository});
-
+  final formKey = GlobalKey<FormState>();
+  final editCtrl = TextEditingController();
+  final chipIndex = 0.obs;
   final tasks = <Task>[].obs;
   @override
   void onInit() {
     super.onInit();
     tasks.assignAll(taskRepository.readTasks());
-    ever(tasks, (_)=>taskRepository.writeTasks(tasks));
-
+    ever(tasks, (_) => taskRepository.writeTasks(tasks));
   }
+
   @override
   void onClose() {
     super.onClose();
+  }
+  void changeChipIndex(int index) {
+    chipIndex.value = index;
   }
 }
--- lib/app/modules/home/widgets/add_card.dart
+++ lib/app/modules/home/widgets/add_card.dart
@@ -21,7 +21,55 @@ class AddCard extends StatelessWidget {
       height: squareWidth / 2,
       margin: EdgeInsets.all(3.0.wp),
       child: InkWell(
-        onTap: () {},
+        onTap: () async {
+          await Get.defaultDialog(
+            titlePadding: EdgeInsets.symmetric(vertical: 5.0.wp),
+            radius: 5,
+            title: 'Task Type',
+            content: Form(
+              child: Column(
+                children: [
+                  Padding(
+                    // 左右+上下
+                    padding:  EdgeInsets.symmetric(horizontal: 3.0.wp),
+                    child: TextFormField(
+                      controller: homeCtrl.editCtrl,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Title',
+                      ),
+                      validator: (value) {
+                        if (value == null || value.trim().isEmpty) {
+                          return 'Please enter a title';
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                 Padding(padding: EdgeInsets.symmetric(vertical: 5.0.wp),
+                 child:  Wrap(
+                    spacing: 2.0.wp,
+                    children:
+                    icons.map((e)=>Obx((){
+                      final index = icons.indexOf(e);
+                      return ChoiceChip(
+                        selectedColor: Colors.grey[200],
+                        pressElevation: 0,
+                        backgroundColor: Colors.white,
+                        label: e,
+                        showCheckmark: false,//不用打勾标记
+                        selected: homeCtrl.chipIndex.value ==index,
+                        onSelected: (bool selected) {
+                          homeCtrl.chipIndex.value = selected?index:0;
+                        }
+                      );
+                    })).toList(),
+                  )
+               ,) ],
+              ),
+            ),
+          );
+        },
         child: DottedBorder(
           color: Colors.grey[400]!,
           dashPattern: [8, 4],
