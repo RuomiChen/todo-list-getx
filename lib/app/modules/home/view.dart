54aa5b34bf47ad3f5196a3b73c5d369ef5342812
zihao.chen <zihao.chen@xilancloud.com>
2025-07-26 11:45:57 +0800

    feat: add TaskCard widget for displaying tasks in HomePage

--- lib/app/modules/home/view.dart
+++ lib/app/modules/home/view.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/app/core/utils/extensions.dart';
+import 'package:flutter_application_1/app/data/models/task.dart';
 import 'package:flutter_application_1/app/modules/home/controller.dart';
 import 'package:flutter_application_1/app/modules/home/widgets/add_card.dart';
+import 'package:flutter_application_1/app/modules/home/widgets/task_card.dart';
 import 'package:get/get.dart';
 
 class HomePage extends GetView<HomeController> {
@@ -23,7 +25,13 @@ class HomePage extends GetView<HomeController> {
             GridView.count(crossAxisCount: 2,
             shrinkWrap: true,
             physics: const ClampingScrollPhysics(),
-            children: [AddCard()],
+            children: [
+              TaskCard(task: Task(
+                title:'title',
+                icon:0xe59c,
+                color: '#ff2b60e6'
+              )),
+              AddCard()],
             )
           ],
         ),
---
+++ lib/app/modules/home/widgets/task_card.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_application_1/app/core/utils/extensions.dart';
+import 'package:flutter_application_1/app/data/models/task.dart';
+import 'package:flutter_application_1/app/modules/home/controller.dart';
+import 'package:get/get.dart';
+import 'package:step_progress_indicator/step_progress_indicator.dart';
+
+class TaskCard extends StatelessWidget {
+  final homeCtrl = Get.find<HomeController>();
+  final Task task;
+
+  TaskCard({super.key, required this.task});
+
+  @override
+  Widget build(BuildContext context) {
+    final color = HexColor.fromHex(task.color);
+    final squareWidth = Get.width - 12.0.wp;
+    return Container(
+      width: squareWidth / 2,
+      height: squareWidth / 2,
+      margin: EdgeInsets.all(3.0.wp),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey[300]!,
+            blurRadius: 7,
+            offset: const Offset(0, 7),
+          ),
+        ],
+      ),
+      child: Column(),
+    );
+  }
+}
