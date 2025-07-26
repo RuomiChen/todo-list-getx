8b5bca43dbf40714f20f7fab4dcad5eabb0dcca0
zihao.chen <zihao.chen@xilancloud.com>
2025-07-26 12:42:30 +0800

    feat: enhance TaskCard widget to display task count and improve layout

--- lib/app/modules/home/widgets/task_card.dart
+++ lib/app/modules/home/widgets/task_card.dart
@@ -30,7 +30,49 @@ class TaskCard extends StatelessWidget {
           ),
         ],
       ),
-      child: Column(),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          StepProgressIndicator(
+            totalSteps: 100,
+            currentStep: 80,
+            size: 5,
+            padding: 0,
+            selectedGradientColor: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [color.withOpacity(0.5), color],
+            ),
+            unselectedGradientColor: const LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [Colors.white, Colors.white],
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(6.0.wp),
+            child: Icon(
+              IconData(task.icon, fontFamily: 'MaterialIcons'),
+              color: color,
+            ),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(task.title,
+              style: TextStyle(fontWeight: FontWeight.bold,
+              fontSize: 12.0.sp),
+              overflow: TextOverflow.ellipsis,),
+              SizedBox(height: 2.0.wp),
+              Text('${task.todos?.length??0} Task',
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.grey
+              ))
+            ],
+          )
+        ],
+      ),
     );
   }
 }
