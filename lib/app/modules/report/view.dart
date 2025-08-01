80b9b091bf4f104de1af4106edebf0e7732d5e8c
zihao.chen <zihao.chen@xilancloud.com>
2025-08-01 10:25:26 +0800

    feat:Circular progress indicator

--- lib/app/modules/report/view.dart
+++ lib/app/modules/report/view.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/app/core/utils/extensions.dart';
+import 'package:flutter_application_1/app/core/values/colors.dart';
 import 'package:flutter_application_1/app/modules/home/controller.dart';
 import 'package:get/get.dart';
 import 'package:intl/intl.dart';
+import 'package:step_progress_indicator/step_progress_indicator.dart';
 
 class ReportPage extends StatelessWidget {
   final homeCtrl = Get.find<HomeController>();
@@ -57,6 +59,46 @@ class ReportPage extends StatelessWidget {
                     _buildStatus(Colors.orange, completedTasks, 'Completed'),
                     _buildStatus(Colors.blue, createdTasks, 'Created'),
                   ],
+                ),
+              ),
+              UnconstrainedBox(
+                child: SizedBox(
+                  width: 70.0.wp,
+                  height: 70.0.wp,
+                  // 第三方库用于分段视觉反馈
+                  child: CircularStepProgressIndicator(
+                    totalSteps: createdTasks == 0 ? 1 : createdTasks,
+                    currentStep: completedTasks,
+                    stepSize: 20,
+                    selectedColor: green,
+                    unselectedColor: Colors.grey[200],
+                    padding: 0,
+                    width: 150,
+                    height: 150,
+                    selectedStepSize: 22,
+                    roundedCap: (_, __) => true,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          '${createdTasks == 0 ? 0 : precent} %',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20.0.sp,
+                          ),
+                        ),
+                        SizedBox(height: 1.0.wp),
+                        Text(
+                          'Efficiency',
+                          style: TextStyle(
+                            color: Colors.grey,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 12.0.sp,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
               ),
             ],
